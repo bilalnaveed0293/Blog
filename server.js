2235c05d642435d642267ae39b88d9f25332de40
Bilal Naveed <bilalnaveed0293@gmail.com>
2024-07-20 18:55:53 +0500

    Create Blog

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Article = require('./models/article')
+const articlerouter = require('./routes/articles')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const app = express();
+
+mongoose.connect('mongodb://localhost/blog');
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt:'desc'});
+   res.render('articles/index' , {articles: articles});
+});
+
+app.use('/articles', articlerouter);
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
